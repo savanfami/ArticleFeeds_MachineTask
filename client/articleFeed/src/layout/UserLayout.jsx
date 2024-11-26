290dfd990bcbecc89bc3e5c2a80fefb69d68a7e5
Savan Fami <savanfami1239@gmail.com>
2024-11-26 11:34:40 +0530

    fix bugsss

--- client/articleFeed/src/layout/UserLayout.jsx
+++ client/articleFeed/src/layout/UserLayout.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import { Navbar } from '../components/header/Navbar'
-import { Signup } from '../pages/user/Signup'
-// import { Outlet } from 'react-router-dom'
 
 export const UserLayout = () => {
   return (
