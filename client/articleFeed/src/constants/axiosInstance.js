624a9ce315302573e004eceba9f2743c092f66ce
Savan Fami <savanfami1239@gmail.com>
2024-11-26 12:04:45 +0530

    fixed env issue

--- client/articleFeed/src/constants/axiosInstance.js
+++ client/articleFeed/src/constants/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 const {VITE_URL} = import.meta.env 
 console.log(VITE_URL,'VITE')
-export const axiosInstance = axios.create({ baseURL: 'articlefeedsmachinetask-production.up.railway.app'});
+export const axiosInstance = axios.create({ baseURL: 'https://articlefeedsmachinetask-production.up.railway.app/api'});
 
 
 axiosInstance.interceptors.response.use(
